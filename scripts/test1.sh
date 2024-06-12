5f295342c15210007cd0cd069412594c9a1f7458
Mihai <git@mehi.dev>
2024-06-12 12:29:07 +0200

    fix typo in test1

--- scripts/test1.sh
+++ scripts/test1.sh
@@ -2,7 +2,7 @@
 
 SUCCESS=true
 for r in $(seq 1 20); do
-    curl -X POST -H "Authorization: Bearer mihai" "http://gatos:8080/api/v1/endpoints/mihai-group-test_mihai-ep-1/external?success=${SUCCESS}";
+    curl -X POST -H "Authorization: Bearer mihai" "http://gatus:8080/api/v1/endpoints/mihai-group-test_mihai-ep-1/external?success=${SUCCESS}";
     sleep 2;
     if [ $r -gt 4 ]; then
         SUCCESS=false
